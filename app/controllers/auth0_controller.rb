bbcc405e5055e3e17919edc7528d9a90f4603314
davidhurley <dhurls99@gmail.com>
2014-11-16 16:09:22 -0500

    making it happen

--- app/controllers/auth0_controller.rb
+++ app/controllers/auth0_controller.rb
@@ -4,7 +4,6 @@ class Auth0Controller < ApplicationController
     name = session[:userinfo][:extra][:raw_info][:nickname]
     insta_id = session[:userinfo][:extra][:raw_info][:identities][0]['user_id']
     Insta.create(nickname: name, insta_id: insta_id)
-
     redirect_to instagram_path
   end
 
