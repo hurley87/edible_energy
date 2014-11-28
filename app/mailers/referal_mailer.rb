2573483c3b6480a562635277bedebb20de5dba82
davidhurley <dhurls99@gmail.com>
2014-11-27 19:19:38 -0500

    making it happeen

--- app/mailers/referal_mailer.rb
+++ app/mailers/referal_mailer.rb
@@ -1,6 +1,6 @@
 class ReferalMailer < ActionMailer::Base
 layout 'referal_mailer'
-attachments.inline['mealnut.png'] = File.read( Rails.root.join("app/some/path/","mealnut.png") )
+attachments.inline['happysocks2.png'] = File.read( Rails.root.join("app/assets/images/","happysocks2.png") )
 
 
  default from: "dhurls99@gmail.com"
