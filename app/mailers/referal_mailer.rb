6b845512237b25b703fa4a4e4c440776d7f4300f
davidhurley <dhurls99@gmail.com>
2014-11-27 19:44:49 -0500

    add images to emails

--- app/mailers/referal_mailer.rb
+++ app/mailers/referal_mailer.rb
@@ -6,6 +6,14 @@ layout 'referal_mailer'
  default from: "dhurls99@gmail.com"
 
  def please_buy(email)
+ 	 attachments.inline['happysocks2.png'] = File.read('app/assets/images/happysocks2.png')
+ 	 attachments.inline['happysocks3.png'] = File.read('app/assets/images/happysocks3.png')
+ 	 attachments.inline['happysocks4.png'] = File.read('app/assets/images/happysocks4.png')
+ 	 attachments.inline['happysocks5.png'] = File.read('app/assets/images/happysocks5.png')
+ 	 attachments.inline['twitter.jpg'] = File.read('twitter.jpg')
+ 	 attachments.inline['insta.jpg'] = File.read('insta.jpg')
+ 	 attachments.inline['facebook.png'] = File.read('facebook.png')
+ 	 attachments.inline['photocool.jpg'] = File.read('photocool.jpg')
 	 mail to: email, subject: "Thanks for your purchase!"
  end
 
