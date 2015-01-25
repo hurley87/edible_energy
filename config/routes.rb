70789441f94095f556ac556e1538dcf7d4d9b486
david hurley <dhurls99@gmail.com>
2015-01-24 23:33:38 -0500

    waht

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
  
 
-resources :user_sessions
-resources :users
+# resources :user_sessions
+# resources :users
 
-get 'login' => 'user_sessions#new', :as => :login
-post 'logout' => 'user_sessions#destroy', :as => :logout
+# get 'login' => 'user_sessions#new', :as => :login
+# post 'logout' => 'user_sessions#destroy', :as => :logout
 
-  get 'instas/new'
+  # get 'instas/new'
 
   get 'auth0/callback'
   get 'auth0/failure'
   get "/auth/auth0/callback" => "auth0#callback"
   get "/auth/failure" => "auth0#failure"
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  # devise_for :admin_users, ActiveAdmin::Devise.config
+  # ActiveAdmin.routes(self)
   
   root 'pages#home'
   get 'terms' => 'pages#terms'
